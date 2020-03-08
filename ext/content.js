1ae711a137c7fdd4af01b2cdb235acf4ba89e884
Eric McKinley <mckinley.eric@yahoo.co.uk>
2020-03-08 12:53:36 +0000

    Ignore free content

--- ext/content.js
+++ ext/content.js
@@ -3,24 +3,24 @@ if(jsonContents && jsonContents.length >= 2) {
     var c1 = jsonContents[0]
     var c2 = jsonContents[1]
 
+
     var o1 = JSON.parse(jsonContents[0].text);
     var o2 = JSON.parse(jsonContents[1].text);
 
     var doccontent = "none";
-
-    if(o1['@type'] == "NewsArticle") {
+    console.dir(o1)
+    if(o1['@type'] == "NewsArticle" && o1['isAccessibleForFree'] == "False") {
       doccontent = o1['articleBody']
     }
-    else if(o2['@type'] == "NewsArticle") {
+    else if(o2['@type'] == "NewsArticle" && o1['isAccessibleForFree'] == "False") {
       doccontent = o2['articleBody']
     }
 
-    if(doccontent != "none") {
+    if(doccontent != "none" ) {
       var div=document.createElement("div");
       document.body.appendChild(div);
       document.body.style.cssText = "height: 100%;overflow-y: hidden"
-      div.style.cssText = "position: fixed;overflow-y: scroll;display: block;width: 90%;top: 25%;left: 5%;max-height: 60%;padding:10px;background-color: rgba(255,255,255,0.95);border: solid 3px green;z-index: 1000;cursor: pointer;font-family: \"Trebuchet MS\", Verdana, sans-serif;";
+      div.style.cssText = "position: fixed;overflow-y: scroll;display: block;width: 90%;top: 25%;left: 5%;max-height: 60%;padding:10px;background-color: rgba(240, 255, 240,0.95);border: solid 3px green;z-index: 1000;cursor: pointer;font-family: \"Trebuchet MS\", Verdana, sans-serif;";
       div.innerText=doccontent;
-      console.log("DONEDIV")
     }
 }
