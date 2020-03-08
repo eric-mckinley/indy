2251d0ea37718b874b99bcc8da84aa8a290bf959
Eric McKinley <mckinley.eric@yahoo.co.uk>
2020-03-08 13:19:46 +0000

    Render unescaped html

--- ext/content.js
+++ ext/content.js
@@ -7,20 +7,22 @@ if(jsonContents && jsonContents.length >= 2) {
     var o1 = JSON.parse(jsonContents[0].text);
     var o2 = JSON.parse(jsonContents[1].text);
 
-    var doccontent = "none";
+    var docContent = "none";
     console.dir(o1)
     if(o1['@type'] == "NewsArticle" && o1['isAccessibleForFree'] == "False") {
-      doccontent = o1['articleBody']
+      docContent = o1['articleBody']
     }
     else if(o2['@type'] == "NewsArticle" && o1['isAccessibleForFree'] == "False") {
-      doccontent = o2['articleBody']
+      docContent = o2['articleBody']
     }
 
-    if(doccontent != "none" ) {
-      var div=document.createElement("div");
-      document.body.appendChild(div);
+    if(docContent != "none" ) {
+      var overlayDiv = document.createElement("div");
+      document.body.appendChild(overlayDiv);
       document.body.style.cssText = "height: 100%;overflow-y: hidden"
-      div.style.cssText = "position: fixed;overflow-y: scroll;display: block;width: 90%;top: 25%;left: 5%;max-height: 60%;padding:10px;background-color: rgba(240, 255, 240,0.95);border: solid 3px green;z-index: 1000;cursor: pointer;font-family: \"Trebuchet MS\", Verdana, sans-serif;";
-      div.innerText=doccontent;
+      overlayDiv.style.cssText = "position: fixed;overflow-y: scroll;display: block;width: 90%;top: 25%;left: 5%;max-height: 60%;padding:10px;background-color: rgba(240, 255, 240,0.95);border: solid 3px green;z-index: 1000;cursor: pointer;font-family: \"Trebuchet MS\", Verdana, sans-serif;";
+
+      var htmlDoc = new DOMParser().parseFromString(docContent, "text/html");
+      overlayDiv.innerHTML = htmlDoc.documentElement.textContent;
     }
 }
