1975d7d324e93195ee8f36156cdff818964abe95
E McKinley <emckinley@es-macbook-pro.home>
2023-09-23 22:14:04 +0100

    updated extension

--- ext/content.js
+++ ext/content.js
@@ -1,50 +1,69 @@
-var jsonContents = $( "script[type*='application/ld+json']" )
-if(jsonContents && jsonContents.length >= 2) {
-    var c1 = jsonContents[0]
-    var c2 = jsonContents[1]
+var articleDataScripts = $( "script[data-fragment-type='ArticleContent']" )
+var introSection = $( "div[data-auth-intro='article']" )
 
+if (introSection && articleDataScripts && articleDataScripts.length > 0) {
 
-    var o1 = JSON.parse(jsonContents[0].text);
-    var o2 = JSON.parse(jsonContents[1].text);
+    var spacer_p = document.createElement("p")
+    spacer_p.style.cssText = "display:block;    "
+    spacer_p.innerHTML = "&nbsp;"
 
-    var docContent = "none";
-    console.dir(o1)
-    if(o1['@type'] == "NewsArticle") {
-      docContent = o1['articleBody']
+    introSection[0].appendChild(spacer_p)
+
+    var p_style = "display:block;font-size:18px;font-family:'Nuacht Serif', 'Nuacht Serif Fallback', Georgia, serif;"
+    var q_style = "font-weight:bold;display:block;font-size:18px;font-family:'Nuacht Serif', 'Nuacht Serif Fallback', Georgia, serif;"
+
+    var scriptContent = articleDataScripts[0].text
+    var jsonContentText = scriptContent.substring(scriptContent.indexOf("{"))
+    var jsonContents = JSON.parse(jsonContentText);
+
+    var premium_button = document.createElement('button')
+    premium_button.innerText = 'View Premium Article'
+    premium_button.style.cssText = "display:block;background-color: #4CAF50;border: none;color: white;padding: 15px 32px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;"
+
+    var p_btn_element = document.createElement("p")
+    p_btn_element.setAttribute("id", "btnindy");
+
+    p_btn_element.style.cssText = p_style
+    p_btn_element.appendChild(premium_button)
+
+    introSection[0].appendChild(p_btn_element)
+
+    premium_button.addEventListener('click', () => {
+      $( '#btnindy' ).fadeOut();
+      $( '#indy' ).fadeIn();
+    })
+
+    var article_div = document.createElement("div")
+    article_div.setAttribute("id", "indy");
+    article_div.style.cssText = "display:none;"
+
+    introSection[0].appendChild(article_div)
+
+    var keys = Object.keys(jsonContents)
+    if (keys && keys.length > 0) {
+         var subData = jsonContents[keys[0]]["data"]["article"]["body"]
+
+         for (const section in subData) {
+            var p_section = subData[section]["p"]
+            if (p_section) {
+
+                var p_element = document.createElement("p")
+                p_element.style.cssText = p_style
+                p_element.innerHTML = p_section
+
+                article_div.appendChild(p_element)
+             }
+
+            var q_section = subData[section]["quote"]
+            if (q_section) {
+
+                var q_element = document.createElement("p")
+                q_element.style.cssText = q_style
+                q_element.innerHTML = q_section["text"];
+                article_div.appendChild(q_element)
+             }
+         }
     }
-    else if(o2['@type'] == "NewsArticle") {
-      docContent = o2['articleBody']
-    }
-
-    if(docContent != "none" ) {
-
-      var docStory = "<strong>Click Box to Close</strong><br><p>"+ docContent + "</p>"
-
-      var overlayDiv = document.createElement("div");
-      overlayDiv.setAttribute("id", "indy");
-      document.body.appendChild(overlayDiv);
-      document.body.style.cssText = "height: 100%;overflow-y: hidden"
-      overlayDiv.style.cssText = "position: fixed;overflow-y: scroll;display: block;width: 90%;top: 25%;left: 5%;max-height: 60%;padding:10px;background-color: rgba(240, 255, 240,0.95);border: solid 3px green;z-index: 1000;cursor: pointer;font-family: \"Trebuchet MS\", Verdana, sans-serif;";
-
-      overlayDiv.innerHTML = htmlEntities(docStory);
-    }
-
-    $( document ).on( 'click', function( event ) {
-      $( '#indy' ).fadeOut();
-  });    
-}
-
-function htmlEntities(str) {
-  return String(str)
-           .replaceAll('&rsquo;', '\'')
-           .replaceAll('&rdquo;', '\"')
-           .replaceAll('&rldquo;', '\"')
-           .replaceAll('&amp;', '&')   // & -> &amp;
-           .replaceAll('&lt;', '<')    // < -> &lt;
-           .replaceAll('&gt;', '>')    // > -> &gt;
-           .replaceAll('&quot;', '\"'); // " -> &quot;
-
-          
 }
 
 
